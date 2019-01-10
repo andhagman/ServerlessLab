cf94d0fe306ca06ab25261d5c9d102fdbe1d182f
Andreas Hagman <Andreas.Hagman@enfogroup.com>
2019-01-10 09:25:24 +0100

    Add a lambda layer

--- src/lambdas/http/get-test.ts
+++ src/lambdas/http/get-test.ts
@@ -1,5 +1,6 @@
 import { lambdaHttpHandler } from '../../utils/lambdaUtils';
 import { ITest } from '../../models/Test';
+// import { v4 } from 'uuid';
 
 export const handler = lambdaHttpHandler(async (event) => {
   const test: ITest = {
@@ -7,6 +8,8 @@ export const handler = lambdaHttpHandler(async (event) => {
     TestNumber: 1,
     TestString: "string"
   }
+
+  // console.log(v4());
 
   return {
     statusCode: 200,
--- src/lambdas/http/post-test.ts
+++ src/lambdas/http/post-test.ts
@@ -1,6 +1,7 @@
 import { lambdaHttpHandler } from '../../utils/lambdaUtils';
 import { validateOrThrow } from '../../utils/validationUtils'
 import * as joi from 'joi';
+//  import { v4 } from 'uuid';
 
 const schema: joi.SchemaLike = joi.object().keys({
     TestString: joi.string().required(),
@@ -9,6 +10,7 @@ const schema: joi.SchemaLike = joi.object().keys({
 });
 
 export const handler = lambdaHttpHandler(async (event) => {
+    // console.log(v4());
 
     const body = JSON.parse(event.body);
 
